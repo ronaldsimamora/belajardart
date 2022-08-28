b533d3b9438b87d1f01ec392aaa21c0967129319
it <mr.ronald.simamora@gmail.com>
2022-08-28 19:47:55 +0700

    comment

--- bin/belajar_dart.dart
+++ bin/belajar_dart.dart
@@ -1,5 +1,19 @@
 import 'package:belajar_dart/belajar_dart.dart' as belajar_dart;
 
+/// Fungsi [main] akan menampilkan 2 output
+/// Output pertama menampilkan teks dan output kedua menampilkan hasil perkalian pada library [belajar_dart]
+///
+
 void main(List<String> arguments) {
-  print('Hello world: ${belajar_dart.calculate()}!');
+  // Mencetak Hello Dart! Dart is great. pada consol
+  print('Hello Dart! Dart is great.');
+
+  // Testing documentation comment with [].
+  print('6 * 7 = ${belajar_dart.calculate()}');
+
+  /*
+  output:
+    Hello Dart! Dart is great.
+    6 * 7 = 42
+   */
 }
