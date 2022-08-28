ea3802184720b5869ce5244518b2432743a48b73
it <mr.ronald.simamora@gmail.com>
2022-08-28 20:32:46 +0700

    comment

---
+++ bin/hello_dart.dart
@@ -0,0 +1,13 @@
+void main() {
+  String greetings = "Hello Dart";
+  print(greetings);
+
+  int myAge;
+  myAge = 20;
+  print(myAge);
+
+  var x;
+  x = 7;
+  x = 'Dart is great';
+  print(x);
+}
---
+++ bin/type_number.dart
@@ -0,0 +1,13 @@
+void main() {
+  var number = 2020;
+  var hex = 0xDEADBEEF;
+  var decimal = 1.2;
+  var pi = 3.14;
+  double withoutDecimal = 7; // Sama dengan double withoutDecimal = 7.0
+
+  print(number);
+  print(hex);
+  print(decimal);
+  print(pi);
+  print(withoutDecimal);
+}
