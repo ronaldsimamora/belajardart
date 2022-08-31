66cce43d6db9a3e1736df9bb918ff8bf197245a0
Ronald Simamora <mail.bl4ckside@gmail.com>
2022-08-31 21:35:18 +0700

    Merge remote-tracking branch 'origin/master'

---
+++ bin/operator_aritmatika.dart
@@ -0,0 +1,16 @@
+void main() {
+  var firstNumber = 4;
+  var secondNumber = 13;
+  var sum = firstNumber + secondNumber;
+  print(sum);
+
+  /*
+  Output:
+  17
+   */
+
+  print(5 + 2); // int add = 7
+  print(5 - 2); // int substract = 3
+  print(5 * 2); // int multiply = 10
+  print(5 / 2); // double divide = 2.5
+}
