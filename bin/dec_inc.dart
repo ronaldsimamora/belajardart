95330f24ffc497b73de48d895096d2d7ba6db090
it <mr.ronald.simamora@gmail.com>
2022-09-02 21:45:46 +0700

    Merge remote-tracking branch 'origin/master'

---
+++ bin/dec_inc.dart
@@ -0,0 +1,7 @@
+void main() {
+  var a = 0, b = 5;
+  a++;
+  b--;
+  print(a); //output = 1
+  print(b); // output = 4
+}
--- bin/operator_aritmatika.dart
+++ bin/operator_aritmatika.dart
@@ -13,4 +13,6 @@ void main() {
   print(5 - 2); // int substract = 3
   print(5 * 2); // int multiply = 10
   print(5 / 2); // double divide = 2.5
+  print(5 ~/ 2); // int intDivide = 2
+  print(5 % 2); // int modulo = 1
 }
